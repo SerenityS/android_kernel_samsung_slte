53abf7368a7b6f57b5510631b39508d3efb3063c
Piotr Stankiewicz <piotrs@opensource.wolfsonmicro.com>
2015-04-02 14:58:33 +0100

    mfd: marley: Initial Marley mfd driver commit
    Change-Id: Ie7ebe4e6fb53d9e7891e3145921062d3a8e2ede2
    Signed-off-by: Piotr Stankiewicz <piotrs@opensource.wolfsonmicro.com>

--- drivers/mfd/arizona-core.c
+++ drivers/mfd/arizona-core.c
@@ -1276,6 +1276,7 @@ const struct of_device_id arizona_of_match[] = {
 	{ .compatible = "wlf,wm1840", .data = (void *)WM1840 },
 	{ .compatible = "wlf,wm1831", .data = (void *)WM1831 },
 	{ .compatible = "cirrus,cs47l24", .data = (void *)CS47L24 },
+	{ .compatible = "cirrus,cs47l35", .data = (void *)CS47L35 },
 	{},
 };
 EXPORT_SYMBOL_GPL(arizona_of_match);
@@ -1340,6 +1341,15 @@ static struct mfd_cell clearwater_devs[] = {
 	{ .name = "arizona-haptics" },
 	{ .name = "arizona-pwm" },
 	{ .name = "clearwater-codec" },
+};
+
+static struct mfd_cell marley_devs[] = {
+	{ .name = "arizona-micsupp" },
+	{ .name = "arizona-extcon" },
+	{ .name = "arizona-gpio" },
+	{ .name = "arizona-haptics" },
+	{ .name = "arizona-pwm" },
+	{ .name = "marley-codec" },
 };
 
 static const struct {
@@ -1507,6 +1517,7 @@ int arizona_dev_init(struct arizona *arizona)
 	case WM1840:
 	case WM1831:
 	case CS47L24:
+	case CS47L35:
 		for (i = 0; i < ARRAY_SIZE(wm5102_core_supplies); i++)
 			arizona->core_supplies[i].supply
 				= wm5102_core_supplies[i];
@@ -1529,6 +1540,7 @@ int arizona_dev_init(struct arizona *arizona)
 	switch (arizona->type) {
 	case WM1831:
 	case CS47L24:
+	case CS47L35:
 		break;
 	default:
 		ret = mfd_add_devices(arizona->dev, -1, early_devs,
@@ -1627,6 +1639,7 @@ int arizona_dev_init(struct arizona *arizona)
 	case 0x6363:
 	case 0x8997:
 	case 0x6338:
+	case 0x6360:
 		break;
 	default:
 		dev_err(arizona->dev, "Unknown device ID: %x\n", reg);
@@ -1806,7 +1819,24 @@ int arizona_dev_init(struct arizona *arizona)
 		apply_patch = clearwater_patch;
 		break;
 #endif
-	default:
+#ifdef CONFIG_MFD_MARLEY
+	case 0x6360:
+		switch (arizona->type) {
+		case CS47L35:
+			type_name = "CS47L35";
+			break;
+
+		default:
+			dev_err(arizona->dev,
+			   "Unknown Marley codec registered as CS47L35\n");
+			arizona->type = CS47L35;
+		}
+
+		revision_char = arizona->rev + 'A';
+		apply_patch = marley_patch;
+		break;
+#endif
+default:
 		dev_err(arizona->dev, "Unknown device ID %x\n", reg);
 		goto err_reset;
 	}
@@ -2093,6 +2123,10 @@ int arizona_dev_init(struct arizona *arizona)
 	case WM1840:
 		ret = mfd_add_devices(arizona->dev, -1, clearwater_devs,
 				      ARRAY_SIZE(clearwater_devs), NULL, 0, NULL);
+		break;
+	case CS47L35:
+		ret = mfd_add_devices(arizona->dev, -1, marley_devs,
+				      ARRAY_SIZE(marley_devs), NULL, 0, NULL);
 		break;
 	}
 
